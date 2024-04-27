c42aedfe6a42685d8bfa5e1d483744b7df074a8c
nyrta1 <nurtai_2005@mail.ru>
2024-04-28 00:57:13 +0500

    feat: create auth dto

---
+++ internal/rest/dto/auth.go
@@ -0,0 +1,14 @@
+package dto
+
+type LoginInput struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type RegisterInput struct {
+	Name     string `json:"name" validate:"required"`
+	Surname  string `json:"surname" validate:"required"`
+	Username string `json:"username" validate:"required"`
+	Email    string `json:"email"`
+	Password string `json:"password" validate:"required"`
+}
