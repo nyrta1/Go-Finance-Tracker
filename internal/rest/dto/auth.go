089fbbe45524949238f4174959ce6ca7107b4d51
nyrta1 <nurtai_2005@mail.ru>
2024-04-28 01:46:37 +0500

    refactor: ...

--- internal/rest/dto/auth.go
+++ internal/rest/dto/auth.go
@@ -9,5 +9,6 @@ type RegisterInput struct {
 	Name     string `json:"name" validate:"required"`
 	Surname  string `json:"surname" validate:"required"`
 	Username string `json:"username" validate:"required"`
+	Email    string `json:"email"  validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
